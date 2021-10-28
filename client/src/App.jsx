44e611bd35af51b99c49066a7eabfdc18b6be187
John Mason <johnmsn22@gmail.com>
2021-10-28 01:38:08 +0100

    search page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Home from './pages/home/Home'
+import Search from './pages/search/Search';
 
 const App = () => {
   return (
@@ -8,6 +9,9 @@ const App = () => {
       <Switch>
         <Route path='/' exact>
           <Home />
+        </Route>
+        <Route path='/search'>
+          <Search />
         </Route>
       </Switch>
     </Router>
